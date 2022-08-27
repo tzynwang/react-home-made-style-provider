f0715d6d88856334ec989e51dbd96141d2b5706b
Tzu Yin <tzyn.wang@gmail.com>
2022-08-27 15:51:38 +0800

    [feat] use StyleToggle in App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,40 +1,16 @@
-/** @jsxImportSource @emotion/react */
-import React, { memo, useState, useMemo } from 'react';
-import coffee from '@Styles/coffee';
-import garden from '@Styles/garden';
+import React, { memo } from 'react';
+import StyleToggle from '@Components/Common/StyleToggle';
 
 function App(): React.ReactElement {
-  /* States */
-  const [styleSet, setStyleSet] = useState<'coffee' | 'garden'>('garden');
-
-  /* Functions */
-  const changeStyle = (): void => {
-    setStyleSet((prev) => (prev === 'coffee' ? 'garden' : 'coffee'));
-  };
-
-  /* Views */
-  const dynamicCss = useMemo(() => {
-    if (styleSet === 'coffee') {
-      return coffee;
-    }
-    if (styleSet === 'garden') {
-      return garden;
-    }
-    return {};
-  }, [styleSet]);
-
   /* Main */
   return (
     <React.Fragment>
-      <button type="button" onClick={changeStyle}>
-        change style
-      </button>
-      <div css={dynamicCss}>
+      <StyleToggle />
+      <div>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam sapiente
         quidem ab autem, quod a facere reiciendis iusto voluptatum maiores
         provident. Eum et voluptas sapiente minus doloremque obcaecati?
         Delectus, repellendus.
-        <button type="button">just a button</button>
       </div>
     </React.Fragment>
   );

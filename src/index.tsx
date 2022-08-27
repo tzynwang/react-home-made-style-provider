c8133b843ee1308576118403966cf855268714db
Tzu Yin <tzyn.wang@gmail.com>
2022-08-27 15:51:55 +0800

    [feat] add default style, StyleProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@Components/App';
+import StyleProvider from '@Components/Layer/StyleProvider';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <StyleProvider>
+      <App />
+    </StyleProvider>
   </React.StrictMode>
 );

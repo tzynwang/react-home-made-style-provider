5905e06af8f6f8198924f7f2c60f5622d854da0e
Tzu Yin <tzyn.wang@gmail.com>
2022-08-27 12:46:29 +0800

    [feat] add styles

---
+++ src/styles/coffee/index.ts
@@ -0,0 +1,16 @@
+import type { DynamicCss } from '../types';
+
+const coffee: DynamicCss = {
+  backgroundColor: '#211720',
+  color: '#d5c8d5',
+  '& button': {
+    border: 'none',
+    padding: '8px 16px',
+    fontFamily: 'inherit',
+    fontSize: 'inherit',
+    backgroundColor: '#dc944c',
+    color: '#3d1f00',
+  },
+};
+
+export default coffee;
---
+++ src/styles/garden/index.ts
@@ -0,0 +1,17 @@
+import type { DynamicCss } from '../types';
+
+const garden: DynamicCss = {
+  backgroundColor: '#e9e7e7',
+  color: '#100f0f',
+  '& button': {
+    border: 'none',
+    padding: '8px 16px',
+    fontFamily: 'inherit',
+    fontSize: 'inherit',
+    backgroundColor: '#5c7f67',
+    color: '#c7ffd9',
+    borderRadius: '8px',
+  },
+};
+
+export default garden;
